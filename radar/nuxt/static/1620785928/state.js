260e81df4454565e974f2e71a6ba3ff9ed51ed7f
TechLabs Aachen GitHub Radar Workflow <aachen@techlabs.org>
2021-05-12 02:19:13 +0000

    Add frontend build at 2021-05-12T02:19:13UTC

---
+++ radar/nuxt/static/1620785928/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fradar\u002Fnuxt\u002Fstatic\u002F1620785928",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fradar\u002F",assetsPath:"\u002Fradar\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"bcd8765a"}},globalRefs:{}}}(null));
